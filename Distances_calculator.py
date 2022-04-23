b0cfd901bb57a2064455ffe099282828a6c69e4d
EduPav <edumarpav@yahoo.com.ar>
2022-04-23 11:04:02 -0300

    23-4 11:03 gral review

--- Distances_calculator.py
+++ Distances_calculator.py
@@ -1,21 +1,29 @@
 import csv
 from a_star import A_Star
-# Funcion que devuelve la coordenada, dado un numero de producto.  coord()
-# Entra maze
-# Could be improved to calculate a triangular matrix, but for simplicity and as its not taking long calculates all
+
+# Could be improved to calculate a triangular matrix, but for simplicity and as its not taking long it calculates all
 
 
-def coord(prod_num, maze):  # Change by oficial coordinates function
+def coord(prod_num, maze):  
+    """
+    Return coordinates(tuple->row,column) in maze of a specified product
+    prod_num: Int->Product number to search for
+    maze: List of lists-> Maze with zeros for possible paths and ints!=0 representing products.  
+    """
     long_list = []
     for elem in maze:
         for value in elem:
             long_list.append(value)
-    c_coord = long_list.index(prod_num) % 13
-    r_coord = int(long_list.index(prod_num)/13)  # Truncates int value
+    c_coord = long_list.index(prod_num) % 13   
+    r_coord = int(long_list.index(prod_num)/13)  #Truncates int value
     return r_coord, c_coord
 
 
-def maze_designer():  # Need 13 blocks. of 8 so we have 104. 4x4=128
+def maze_designer():  # We Need at least 13 blocks of 8 products so we have 104>99. We choose 4x4=128 products design
+    """
+    Return maze: List of lists-> Maze with zeros for possible paths and ints!=0 representing products. 
+    Build it like a 4x4 group of blocks with a size of 4x2 of single width between them.
+    """
     maze = []
     for i in range(4*5+1):
         maze.append([])
@@ -32,35 +40,48 @@ def maze_designer():  # Need 13 blocks. of 8 so we have 104. 4x4=128
     return maze
 
 
-def closest_valid_position(prod_nb, pos):
-    """Returns the closest valid position of a product"""
+def closest_valid_position(prod_num, pos): 
+    """
+    Return the closest valid position (tuple->row,column) to pick up a product
+    prod_num: Int->Product number to search for 
+    pos: tuple (row,coord)->Product coordinates
+    """
     r_coord, c_coord = pos
-    if prod_nb%2 == 0:
-        c_coord += 1
+    if prod_num%2 == 0:
+        c_coord += 1    #Even products are picked up from their right
     else:
-        c_coord -= 1
+        c_coord -= 1    #Odd products are picked up from their left
     return r_coord, c_coord
 
 
 
+def main():
+    """
+    Design of the shortest distances matrix between each pair of products.
+    Write output to "Distance_matrix.csv" archive
+    """
+    maze = maze_designer()
 
-maze = maze_designer()
+    nprod = 99  # Number of products (according to orders archive)
+    distance_matrix = []
+    for i in range(nprod):  # Complexity n^2 being n the number of products in the facility
+        distance_matrix.append([])  #This line adds a row.
+        # i references product number i+1
+        for j in range(nprod):
+            #j references product number j+1
+            if i==j: #No need to run A_Star to get distance of a product to itself
+                distance_matrix[i].append(1)
+                continue
+            coord_pickup_a=closest_valid_position(i+1,coord(i+1, maze)) #a is product i+1
+            coord_pickup_b=closest_valid_position(j+1,coord(j+1, maze)) #b is product j+1
+            ab_path_len = len(A_Star(maze, coord_pickup_a, coord_pickup_b))
+            distance_matrix[i].append(ab_path_len) #Add one distance to current row
 
-nprod = 99  # Number of products
-distance_matrix = []
-for i in range(nprod):  # Complexity n^2 being n the number of products in the facilitiy
-    distance_matrix.append([])  # It becomes a list of lists
-    # i references product number i+1
-    for j in range(nprod):
-        if i==j:
-            distance_matrix[i].append(1)
-            continue
-        coord_a=closest_valid_position(i+1,coord(i+1, maze))
-        coord_b=closest_valid_position(j+1,coord(j+1, maze))
-        ABpath_len = len(A_Star(maze, coord_a, coord_b))
-        distance_matrix[i].append(ABpath_len)
+    #Write distance_matrix list of lists to the archive
+    with open("distance_matrix.csv", "w", newline='') as f:
+        wr = csv.writer(f)
+        wr.writerows(distance_matrix)
 
 
-with open("distance_matrix.csv", "w", newline='') as f:
-    wr = csv.writer(f)
-    wr.writerows(distance_matrix)
+if __name__ == '__main__':
+    main()
--- Simulated_Annealing.py
+++ Simulated_Annealing.py
@@ -1,83 +1,113 @@
-from asyncore import read
 import random
 import math
 import csv
 
 #As we read distance matrix from archive we don't need a_star or the maze here.
-#We only receive lists of proucts and know where they are in the maze.
+#We only receive lists of products and know where they are in the maze.
 #Try changing number of total iterations in sim anealing to see if it's worth getting to that low temperature.
 #Be careful with temperature. It might have certain relationship with the real cost, or the cost evolution.
 #We will have to print costs vs iterations
 #Add input parameters in docstrings at start of each function
 #Start variables and functions with lowercase (search for pep8) and snake notation
-
-#dictionary = {1:[0,1], 2:[0,2], 3:[2,0], 4:[2,3], 5:[3,0], 6:[3,3], 7:[4,0], 8:[4,3], 9:[6,0], 10:[6,3], 11:[7,0], 12:[7,3], 13:[8,0], 14:[8,3], 15:[9,0], 16:[9,3], 17:[11,0], 18:[11,3], 19:[12,0], 20:[12,3], 21:[13,0], 22:[13,3], 23:[14,0], 24:[14,3]}
-#Despues cambiar dictionary (ahora esta puesto para que no de error)
+#REVISAR PROBABILIDAD DE ACEPTAR UN VECINO MALO
+#Write all docstrings in infinitive
+#Print the maze at least once? Without the paths.
+#Change cooling schedule (temperature function) #Nos interesa menos que lineal si la superficie es escarpada. Arrancar con lineal. Si es erratico es escarpada.
+#Could avoid entering number of iteration if makes T=T*0,99
 
 def read_file(filename, order_number):
-    """Read file to get product list"""
+    """
+    Read "filename" file to return the "order_number"º products list
+    filename: string->Name of the file to read
+    order_number: int->Number of order to read from the file.
+    """
     result = []
     tmp = []
-    if order_number <= 100:
+    if order_number >= 0 and order_number <= 100:
         command = "Order " + str(order_number)
         with open(filename, 'r') as f:
             for line in f:
-                tmp.append(line.strip())
-        index_command = tmp.index(command)+1
-        tmp = tmp[index_command:]
+                tmp.append(line.strip()) #Build list of every line in the archive
+        index_command = tmp.index(command)+1 #Get the index where the order specified starts
+        tmp = tmp[index_command:]   #Cut from the list the previous lines to the specified order
         i = 0
         while i < len(tmp):
             if(tmp[i] == ''):
                 break
             i+=1
-        tmp = tmp[0:i]
+        tmp = tmp[0:i] #Now tmp is the list of products in the specified order
         for elem in tmp:
-            result.append(int(elem.replace("P", "")))
+            result.append(int(elem.replace("P", ""))) #Transform every product from string "Px" into int x
         return result
     else:
         print("Order doesn't exist")
         return False
 
 
-def Random_Permutation(order_list):
-    """Asks for an sequence. Returns a neighbour sequence with one permutation"""
-    random_a = random.randint(1, len(order_list)-1)
+def random_permutation(order_list):
+    """
+    Return a neighbour sequence (list) with one permutation of the input one.
+    order_list: list->Sequence of products 
+    """
+    random_a = random.randint(1, len(order_list)-1)  
     random_b = random.randint(1, len(order_list)-1)
+    while(random_a==random_b): #So it doesn't pick the same product in the sequence twice
+        random_b = random.randint(1, len(order_list)-1)
 
-    order_a = order_list[random_a]
-    order_b = order_list[random_b]
 
-    order_list[random_b] = order_a
-    order_list[random_a] = order_b
+    prod_a = order_list[random_a]
+    prod_b = order_list[random_b]
+
+    order_list[random_b] = prod_a
+    order_list[random_a] = prod_b
 
     return order_list
 
-def temperature(TO, k): #Nos interesa menos que lineal si la superficie es escarpada. Arrancar con lineal. Si es erratico es escarpada.
-    """Asks for the initial temperature and the current iteration. Returns reduced temperature"""
+def temperature(TO, k): 
+    """
+    Define cooling schedule. 
+    Return new temperature given the number of iteration and T0
+    T0: int-> starting temperature
+    k: int-> number of iteration
+    """
     T = TO*pow((0.99),k) #0.99 is the cooling rate T0*0,99^k
     return T
 
-def probability(New_Cost, current_Cost, T):
-    """Asks for the new and current cost of the picking secuence and the temperature. Returns the probability of accepting a bad neighbor"""
-    prob = math.exp(-(New_Cost-current_Cost)/T)
+def probability(new_cost, current_cost, T):
+    """
+    Return int-> probability of accepting a worse neighbor
+    new_cost: int-> cost of neighbour sequence
+    current_cost: int-> cost of current sequence
+    """
+    prob = math.exp(-(new_cost-current_cost)/T)
     return prob
 
-def Total_cost_of (sequence, distances):
-    """Returns the picking sequence cost"""
-    Tcost=0
+def total_cost_of (sequence, distances):
+    """
+    Return    int->picking up sequence total cost
+    sequence  list-> sequence of products
+    distances list of lists->distance_matrix with lower costs of traveling between each pair of products
+    """
+    total_cost=0
     for i in range(len(sequence)-1):        
-        Tcost+=distances[sequence[i]][sequence[i+1]]
-    return Tcost
+        total_cost+=distances[sequence[i]][sequence[i+1]]
+    return total_cost
 
 
-def Simulated_Annealing(distances, sequence,T0,Kmax):
-    """Returns the best picking sequence"""
+def simulated_annealing(distances, sequence,T0,Kmax):
+    """
+    Return list->Best picking sequence found
+    distances: list of lists->distance_matrix with lower costs of traveling between each pair of products
+    sequence:  list-> starting sequence
+    T0: int->starting temperature
+    Kmax: int->max number of iterations
+    """
     T=T0
     for counter in range(Kmax):
-        previous_cost=Total_cost_of(sequence,distances)
-        New_sequence = Random_Permutation(sequence)  #Generate a neighbor
+        previous_cost=total_cost_of(sequence,distances)
+        New_sequence = random_permutation(sequence)  #Generate a neighbor
         
-        current_cost = Total_cost_of(New_sequence, distances)
+        current_cost = total_cost_of(New_sequence, distances)
 
         if current_cost < previous_cost:
             sequence = New_sequence
@@ -99,7 +129,7 @@ def Simulated_Annealing(distances, sequence,T0,Kmax):
 def main():
     #it=input("Insert number of random initializations")
     #In case we want to run it with random initializations
-    with open('Distance_matrix.csv') as csvfile:
+    with open('distance_matrix.csv') as csvfile:
         rows = csv.reader(csvfile)
         Distance_matrix = list(zip(*rows))
     #Distance matrix is now a list of tuples
@@ -109,7 +139,7 @@ def main():
     
     Kmax = 10000 #Maximum number of iterations
     Temp0 = 10 #INITIAL TEMPERATURE
-    Simulated_Annealing(Distance_matrix,order,Temp0,Kmax)
+    simulated_annealing(Distance_matrix,order,Temp0,Kmax)
     print(Distance_matrix)
     print("This is the main")
     print("The sequence is:")
@@ -118,4 +148,3 @@ def main():
 
 if __name__ == '__main__':
     main()
-#REVISAR PROBABILIDAD DE ACEPTAR UN VECINO MALO
