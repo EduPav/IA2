36b1684e6b6e689ffc2d417ca5e9e840b374603a
EduPav <edumarpav@yahoo.com.ar>
2022-04-28 11:17:04 -0300

    Group review

--- Genetic_Algorithm.py
+++ Genetic_Algorithm.py
@@ -1,116 +1,125 @@
 import random
 import math
+# Correct language use
+# pep-8
+# Klaga: Build generate random individual function of python so it's more efficient.
+# Si cambias tamaño de la poblacion, cambiar population_premium.
+# Use premium inside crossover function. (Be careful. Crossover strongly depends on the population size)
+# Corrige Agus para cruce de orden.
+
 
 order_one = [10, 5, 8, 9, 2, 7, 1, 3, 6, 4]
 order_two = [3, 5, 8, 7, 9, 4, 6, 1, 10, 2]
 
-def generate_warehouse_layout(lista_original):
+
+def generate_random_individual(lista_original):
 
     lista = lista_original[:]
     longitud_lista = len(lista)
     for i in range(longitud_lista):
-        
+
         indice_aleatorio = random.randint(0, longitud_lista - 1)
         # Exchange
         temporal = lista[i]
         lista[i] = lista[indice_aleatorio]
         lista[indice_aleatorio] = temporal
-    
+
     return lista
 
-def fitness(population):
 
+def fitness(population_size):
     pass
 
-def Population_Premium(population_layout, population_cost):  #Elimina los últimos 2 valores
 
-    result_list = [i for _,i in sorted(zip(population_cost,population_layout))]  #Ordena population_layout de acuerdo a population_cost
-    result_cost = sorted(population_cost)
+def population_premium(population, population_costs):  # Elimina los últimos 2 valores
 
-    population_layout = result_list[::-1]
-    population_cost = result_cost[::-1]
+    # Ordena population de acuerdo a population_costs
+    temp_list = [i for _, i in sorted(zip(population_costs, population))]
+    temp_cost = sorted(population_costs)
 
-    for i in range(2):
-        min = population_cost.index(min(population_cost))
-        population_cost.pop(min)
-        population_layout.pop(min)
+    population = temp_list  # [::-1]
+    population_costs = temp_cost  # [::-1]
 
-    population_layout.append(population_layout[0])
-    population_layout.append(population_layout[3])
+    for _ in range(2):
+        min_idx = population_costs.index(min(population_costs))
+        population_costs.pop(min_idx)
+        population.pop(min_idx)
 
-    population_cost.append(population_cost[0])
-    population_cost.append(population_cost[3])
+    population.append(population[0])
+    population.append(population[3])
 
-    return population_layout, population_cost
+    population_costs.append(population_costs[0])
+    population_costs.append(population_costs[3])
 
-def crossover(population_layout):
-    hijos = []
-    for i in range( 0, len(population_layout), 2):
-        hijo1 = [0]*len(population_layout[0])
-        hijo2 = [0]*len(population_layout[0])
-        a = random.randint(0, len(population_layout[0])-1)
-        b = random.randint(a, len(population_layout[0])-1)
-        auxA = population_layout[i][a:b+1]
-        auxB = population_layout[i+1][a:b+1]
+    return population, population_costs
 
-        hijo1[a:b] = auxB
-        hijo1[a:b] = auxA
+
+def crossover(population):
+    juniors = []
+    for i in range(0, len(population), 2):
+        individual_length = len(population[0])
+        junior1 = [0]*individual_length
+        junior2 = [0]*individual_length
+
+        a = random.randint(0, individual_length-2)
+        b = random.randint(a+1, individual_length-1)
+        auxA = population[i][a:b]
+        auxB = population[i+1][a:b]
+
+        junior1[a:b] = auxB
+        junior2[a:b] = auxA
         j = 0
-        while j < (len(population_layout[0])):
-            
-            if j<a or j>b:
-                numA = random.randint(0, len(population_layout[0])-1)
-                numB = random.randint(0, len(population_layout[0])-1)
-                if numA not in hijo1 and numB not in hijo2:
-                    hijo1[j]=numA
-                    hijo2[j]=numB
-                    j= j + 1
-            
-            else: 
-                j = j+1
-        hijos.append(hijo1)
-        hijos.append(hijo2)
-    
+        while j < (individual_length):
+
+            if j < a or j > b:  
+                numA = random.randint(1, individual_length-1)
+                numB = random.randint(1, individual_length-1)
+                if numA not in junior1 and numB not in junior2:
+                    junior1[j] = numA
+                    junior2[j] = numB
+                    j = j + 1
+
+            else:
+                j = j + 1
+        juniors.append(junior1)
+        juniors.append(junior2)
+
 
 def mutation(individual):
     random_a = random.randint(0, len(individual)-1)
     random_b = random.randint(0, len(individual)-1)
-    #print(random_a)
-    order_a = individual[random_a]
-    order_b = individual[random_b]
+    # print(random_a)
+    individual_a = individual[random_a]
+    individual_b = individual[random_b]
 
-    individual[random_b] = order_a
-    individual[random_a] = order_b
+    individual[random_b] = individual_a
+    individual[random_a] = individual_b
 
     return individual
 
+
 def main():
-    time = 1000 #Max amount of iterations
+    time = 1000  # Max amount of iterations
 
     original_layout = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-    population = 6
-    population_layout = []
-    population_cost = []
-    mutation_prob = 0.01 #mutation probability
+    population_size = 6
+    population = []
+    population_costs = []
+    mutation_prob = 0.01  # mutation probability
 
+    # Genera la población
+    for i in range(population_size):
+        population.append(generate_random_individual(original_layout))
 
-    #Genera la población
-    for i in range(population):
-        population_layout.append(generate_warehouse_layout(original_layout))
-    
-    print('\n'.join([''.join(['{:4}'.format(item) for item in row]) 
-      for row in population_layout]))  #Print the matrix
+    print('\n'.join([''.join(['{:4}'.format(item) for item in row])
+                     for row in population]))  # Print the matrix
 
+    # Calcula el "costo" de cada ordenamiento
+    for i in range(population_size):
+        population_costs.append(fitness(population[i]))
 
-    #Calcula el "costo" de cada ordenamiento
-    for i in range(population):
-        population_cost[i].append(fitness(population_layout[i]))
-
-    
-    population_layout, population_cost = Population_Premium(population_layout, population_cost) #Elimina los últimos 2 valores
-
-
-
+    population, population_costs = population_premium(
+        population, population_costs)  # Elimina los últimos 2 valores
 
 
 if __name__ == '__main__':
--- test.py
+++ test.py
@@ -1,2 +1,16 @@
+import random
+def mutation(individual):
+    random_a = random.randint(0, len(individual)-1)
+    random_b = random.randint(0, len(individual)-1)
+    # print(random_a)
+    individual_a = individual[random_a]
+    individual_b = individual[random_b]
 
+    individual[random_b] = individual_a
+    individual[random_a] = individual_b
+
+    return individual
+a=[1,2,3,4]
+mutation(a)
+print(a)
 print("Develop branch")
